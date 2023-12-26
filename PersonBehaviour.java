d7b99bce933632a949fb69cdf887ace03f9f36b9
avk1702 <avk1702@bk.ru>
2023-12-26 12:31:08 +0500

    1

---
+++ PersonBehaviour.java
@@ -0,0 +1,9 @@
+package Java.OOP_2;
+
+public interface PersonBehaviour {
+    void setMakeOrder(boolean makeOrder);
+    void setTakeOrder(boolean takeOrder);
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+    
+}
